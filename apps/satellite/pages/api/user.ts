60b632ebda4bd8cb590aaae449e502bfb6844806
George Desipris <george.desipris.56@gmail.com>
2023-09-08 17:55:50 +0300

    fix: Return user from api route

--- apps/satellite/pages/api/user.ts
+++ apps/satellite/pages/api/user.ts
@@ -1,7 +1,11 @@
-import { getAuth } from "@clerk/nextjs/server";
+import { clerkClient, getAuth } from "@clerk/nextjs/server";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { user } = getAuth(req);
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { userId } = getAuth(req);
+  const user = userId ? await clerkClient.users.getUser(userId) : null;
   return res.status(200).json(user);
 }
